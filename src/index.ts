a58e53015040ec13ef04466b9a2339cef6a0c650
Marlapati Venkata Naga Sai Teja[marlapativ] <marlapati.v@northeastern.edu>
2024-03-29 11:48:17 -0400

    Removing unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { CloudEvent, cloudEvent } from '@google-cloud/functions-framework'
 import { UserAddCloudEvent, userService } from './user.service'
 import env from './config/env'
-import sendGridClient from '@sendgrid/mail'
 import database from './config/database'
 
 const initializeApp = async () => {
